21ebf9af26b2bed0e06ec7ab4783c87b41151d06
Christina Kt <xriskt@gmail.com>
2022-01-07 02:12:40 +0200

    Trying to add some functionality to Password Change

--- Web App/change_psw.php
+++ Web App/change_psw.php
@@ -25,7 +25,54 @@
       <div class="input-group">
         <input class="btn" type="submit" name="change" value="Submit">
       </div>
-      
+
     </form>
+
+    <script type="text/javascript">
+        function validatePassword() {
+
+        var password  = true;
+        var npassword = true;
+        var cpassword = true;
+        var output    = true;
+
+        password  = document.frmChange.password;
+        npassword = document.frmChange.npassword;
+        cpassword = document.frmChange.cpassword;
+
+        if(!password.value) {
+          password.focus();
+          document.getElementById("password").innerHTML = "required";
+          output = false;
+        }
+
+        else if(!npassword.value) {
+          npassword.focus();
+          document.getElementById("npassword").innerHTML = "required";
+          output = false;
+        }
+
+        else if(!cpassword.value) {
+          cpassword.focus();
+          document.getElementById("cpassword").innerHTML = "required";
+          output = false;
+        }
+
+        if(npassword.value != cpassword.value) {
+          npassword.value="";
+          cpassword.value="";
+          npassword.focus();
+          document.getElementById("cpassword").innerHTML = "not same";
+          output = false;
+        }
+
+        return output;
+        }
+      </script>
+
+
+
+
+
   </body>
 </html>
